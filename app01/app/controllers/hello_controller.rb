56fbe61c189bcbd90643fcb1f867d18fc24abd9e
tatsuya-furudate <marifuru0902@gmail.com>
2016-12-19 20:00:20 +0900

    edit hello

--- app01/app/controllers/hello_controller.rb
+++ app01/app/controllers/hello_controller.rb
@@ -1,5 +1,21 @@
 class HelloController < ApplicationController
     def index
-        render plain:'hello'
+    end
+    def param
+        if params['msg'] != nil then
+            msg = 'hello, ' + params['msg'] + '!'
+        else
+            msg = 'this is sample page.'
+        end
+
+       html = '
+       <html>
+       <body>
+           <h1>sample page</h1>
+           <p>' + msg + '</p>
+       </body>
+       </html>
+       '
+        render html: html.html_safe
     end
 end
--- app01/config/routes.rb
+++ app01/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'hello/index'
     get 'hello', to: 'hello#index'
+    get 'hello/param'
+    get 'hello', to: 'hello#param'
 end
