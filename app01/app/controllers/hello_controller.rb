9f331efb6feeab7f2d464f44ce17a4b5a7fb0800
tatsuya-furudate <marifuru0902@gmail.com>
2016-12-24 14:53:39 +0900

    helo

--- app01/app/controllers/hello_controller.rb
+++ app01/app/controllers/hello_controller.rb
@@ -1,5 +1,16 @@
 class HelloController < ApplicationController
+    protect_from_forgery
+
     def index
+        if request.post? then
+          @title = 'Result'
+          @msg = 'you typed: ' + params['input1'] + '.'
+          @value = params['input1']
+        else
+          @title = 'Index'
+          @msg= 'type text....'
+          @value=''
+        end
     end
     def param
         if params['msg'] != nil then
